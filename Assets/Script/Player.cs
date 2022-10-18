43f9f9404ff3dd39a02dd3b73195411e419355b6
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-10-18 14:54:49 +0900

    プロビルダーを導入。プレイヤーキャラを作成。プレイヤーのジャンプ機能を追加。プレイヤーの移動時に進行方向を向くようにし、移動用スクリプトを調整。

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,14 +5,15 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float _speed = 5;
-    [SerializeField] bool _right = false;
-
+    [SerializeField] float _speed = 5;      //移動速度
+    [SerializeField] float _jumpPower = 3;  //ジャンプ力
+    [SerializeField] bool _left = true;    //左のキャラかどうか
+    Rigidbody _rb;
     // Start is called before the first frame update
     Vector2 _stickValue;
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,15 +26,39 @@ public class Player : MonoBehaviour
             return;
         }
         
-        if(!_right)
+        //移動
+        if(_left)
         {
-            _stickValue = gamepad.leftStick.ReadValue().normalized;
-
+            _stickValue = gamepad.leftStick.ReadValue().normalized; 
         }
         else
         {
             _stickValue = gamepad.rightStick.ReadValue().normalized;
         }
-        transform.Translate(_stickValue.x*_speed * Time.deltaTime,0,_stickValue.y * _speed * Time.deltaTime);
+        if(_stickValue !=  new Vector2(0,0))
+        {
+            var dir = new Vector3(_stickValue.x,0,_stickValue.y);
+            transform.localRotation = Quaternion.LookRotation(dir);
+            transform.Translate(0, 0,_speed * Time.deltaTime);
+            //transform.Translate(_stickValue.x * _speed * Time.deltaTime, 0, _stickValue.y * _speed * Time.deltaTime);
+        }
+        
+        
+        //ジャンプ
+        if (_left)
+        {
+            if (gamepad.leftShoulder.wasPressedThisFrame)
+            {
+                _rb.AddForce(0, _jumpPower, 0, ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            if(gamepad.rightShoulder.wasPressedThisFrame)
+            {
+                _rb.AddForce(0,_jumpPower,0,ForceMode.Impulse);
+            }
+        }
+
     }
 }
