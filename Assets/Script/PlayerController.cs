e886d82ce9d1bec75e527480b76f1bce7b024b3a
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-11-08 14:30:28 +0900

    PlayerAttackのスクリプトをPlayerControllerに統合 回避とダッシュのスクリプトを追加

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,17 +5,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float _speed = 5;      //移動速度
+    int _skillIndex = 0;
+    [SerializeField] float _speed = 5;      //現在の移動速度
+    [SerializeField] float _defaultSpeed = 5; //移動速度の初期値
+    [SerializeField] float _dashSpeed = 10; //移動速度の初期値
     [SerializeField] float _jumpPower = 3;  //ジャンプ力
     [SerializeField] bool _left = true;    //左のキャラかどうか
+    [SerializeField] int _avoid = 3;//回避の速度
+    [SerializeField] float _avoidCoolTime = 1.5f; //回避のクールタイム
+
+
     Rigidbody _rb = default;
+
     // Start is called before the first frame update
     Vector2 _stickValue = default;
-    PlayerAttack _attack = default;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _attack = GetComponent<PlayerAttack>();
     }
 
     // Update is called once per frame
@@ -37,14 +43,37 @@ public class PlayerController : MonoBehaviour
             //ジャンプ
             if (gamepad.leftShoulder.wasPressedThisFrame) _rb.AddForce(0, _jumpPower, 0, ForceMode.Impulse);
 
-            //攻撃
-            if (gamepad.leftTrigger.wasPressedThisFrame) _attack.Attack();
+            
 
             //スキル変更
-            if (gamepad.dpad.up.wasPressedThisFrame) _attack.SkillChange(0); //方向キー上
-            if (gamepad.dpad.right.wasPressedThisFrame) _attack.SkillChange(1); //方向キー右
-            if (gamepad.dpad.left.wasPressedThisFrame) _attack.SkillChange(2); //方向キー左
-            if (gamepad.dpad.down.wasPressedThisFrame) _attack.SkillChange(3); //方向キー下
+            if (gamepad.dpad.up.wasPressedThisFrame) _skillIndex = 0; //方向キー上
+            if (gamepad.dpad.right.wasPressedThisFrame) _skillIndex = 1; //方向キー右
+            if (gamepad.dpad.left.wasPressedThisFrame) _skillIndex = 2; //方向キー左
+            if (gamepad.dpad.down.wasPressedThisFrame) _skillIndex = 3; //方向キー下
+
+            if (gamepad.leftTrigger.wasPressedThisFrame && _skillIndex == 0)
+            {
+                Debug.Log("回復");
+            }
+            else if (gamepad.leftTrigger.wasPressedThisFrame && _skillIndex == 1)
+            {
+                Debug.Log("ダッシュ");
+                _rb.AddForce(transform.forward * _avoid,ForceMode.Impulse);
+                _speed = _dashSpeed;
+            }
+            else if (gamepad.leftTrigger.wasPressedThisFrame && _skillIndex == 2)
+            {
+                Debug.Log("攻撃");
+            }
+            else if (gamepad.leftTrigger.isPressed && _skillIndex == 3)
+            {
+                Debug.Log("ガード");
+            }
+            else if (gamepad.leftTrigger.wasReleasedThisFrame)
+            {
+                _speed = _defaultSpeed;
+            }
+
         }
         else//右
         {
@@ -55,14 +84,11 @@ public class PlayerController : MonoBehaviour
             if (gamepad.rightShoulder.wasPressedThisFrame) _rb.AddForce(0, _jumpPower, 0, ForceMode.Impulse);
 
 
-            //攻撃
-            if (gamepad.rightTrigger.wasPressedThisFrame) _attack.Attack();
-
             //スキル変更
-            if (gamepad.buttonNorth.wasPressedThisFrame) _attack.SkillChange(0); //PSだと△
-            if (gamepad.buttonWest.wasPressedThisFrame) _attack.SkillChange(1); //PSだと□
-            if (gamepad.buttonEast.wasPressedThisFrame) _attack.SkillChange(2); //PSだと○
-            if (gamepad.buttonSouth.wasPressedThisFrame) _attack.SkillChange(3); //PSだと×
+            if (gamepad.buttonNorth.wasPressedThisFrame) _skillIndex = 0; //PSだと△
+            if (gamepad.buttonWest.wasPressedThisFrame) _skillIndex = 1; //PSだと□
+            if (gamepad.buttonEast.wasPressedThisFrame) _skillIndex = 2; //PSだと○
+            if (gamepad.buttonSouth.wasPressedThisFrame) _skillIndex = 3; //PSだと×
         }
 
         // 移動
@@ -73,7 +99,5 @@ public class PlayerController : MonoBehaviour
             transform.Translate(0, 0, _speed * Time.deltaTime);
 
         }
-
-
     }
 }
